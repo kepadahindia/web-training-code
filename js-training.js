8ddc3574f814b7ac3ea772df14b98723fa31b7b7
Oscar Reizo Aprilliano <oscareizo2009@gmail.com>
2026-01-20 16:19:31 +0700

    Upload pertama saya

---
+++ js-training.js
@@ -0,0 +1,5 @@
+const projectBtn = document.getElementById("project-btn"); // Mengambil elemen tombol berdasarkan ID
+
+projectBtn.addEventListener("click", () => { // Menambahkan event listener untuk tombol, itu juga ada tombol ARROW FUNCTION, Saat tombol diklik, jalankan isi fungsi ini.
+  alert("This is my first frontend portfolio project 🚀"); //AKSI YANG DIJALANKAN
+});
